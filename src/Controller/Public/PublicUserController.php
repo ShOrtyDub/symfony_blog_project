3a891f943d26b205036f0d27d734528af616a715
ShOrtyDub <rakoto.mathieu@gmail.com>
2023-08-01 21:14:05 +0200

    maj navbar, sidebar dans home

--- src/Controller/Public/PublicUserController.php
+++ src/Controller/Public/PublicUserController.php
@@ -36,8 +36,8 @@ class PublicUserController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-//            return $this->redirectToRoute('user_login', [], Response::HTTP_SEE_OTHER);
-            return $this->render('account/login/account_login.html.twig', ['last_username' => $user->getEmail(), 'error' => []]);
+            return $this->redirectToRoute('user_login', [], Response::HTTP_SEE_OTHER);
+//            return $this->render('account/login/account_login.html.twig', ['last_username' => $user->getEmail(), 'error' => []]);
 
         }
 
