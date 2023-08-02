199a68fdbe403becbe707197c1656854194cb609
ShOrtyDub <rakoto.mathieu@gmail.com>
2023-08-02 16:56:56 +0200

    maj edit profil

--- src/Controller/Account/AccountController.php
+++ src/Controller/Account/AccountController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller\Account;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,4 +22,45 @@ class AccountController extends AbstractController
             'controller_name' => 'AccountController',
         ]);
     }
+
+    #[Route('/profil/{id}', name: 'app_account_profil')]
+    public function profil(User $user): Response
+    {
+        return $this->render('account/profil/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    //TODO
+    #[Route('/profil/{id}/edit', name: 'app_account_profil_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+//        $user = $userRepository->find($security->getUser()->getId());
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_account', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('account/profil/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_account_profil_delete', methods: ['POST'])]
+    public function delete(Request $request, Commentaires $commentaire, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$commentaire->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($commentaire);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_account_commentaires_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+
 }
