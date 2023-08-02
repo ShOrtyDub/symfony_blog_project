4067a07d29d123c82d99bcd0fc836c2aa3ca8717
ShOrtyDub <rakoto.mathieu@gmail.com>
2023-08-02 09:56:34 +0200

    maj public accès categories et articles

---
+++ src/Controller/Public/PublicArticlesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Public;
+
+use App\Entity\Articles;
+use App\Form\ArticlesType;
+use App\Repository\ArticlesRepository;
+use App\Service\FileUploaderService;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('account/public/articles')]
+class PublicArticlesController extends AbstractController
+{
+    #[Route('/list/{FK_categories?}', name: 'app_public_articles_index', methods: ['GET'])]
+    public function index(ArticlesRepository $articlesRepository, $FK_categories): Response
+    {
+        if ($FK_categories === null) {
+            return $this->render('account/articles/index.html.twig', [
+                'articles' => $articlesRepository->findAll(),
+            ]);
+        } else {
+            return $this->render('account/articles/index.html.twig', [
+                'articles' => $articlesRepository->findBy(['FK_categories' => $FK_categories]),
+
+            ]);
+        }
+
+    }
+
+    #[Route('/{id}', name: 'app_public_articles_show', methods: ['GET'])]
+    public function show(Articles $article): Response
+    {
+        return $this->render('account/articles/show.html.twig', [
+            'article' => $article,
+            'categorie' => $article->getFKCategories()->getNom()
+        ]);
+    }
+
+}
--- src/Controller/Public/PublicCategoriesController.php
+++ src/Controller/Public/PublicCategoriesController.php
@@ -22,60 +22,11 @@ class PublicCategoriesController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_categories_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $category = new Categories();
-        $form = $this->createForm(CategoriesType::class, $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($category);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_public_categories_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('admin/categories/new.html.twig', [
-            'category' => $category,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_categories_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_public_categories_show', methods: ['GET'])]
     public function show(Categories $category): Response
     {
-        return $this->render('admin/categories/show.html.twig', [
+        return $this->render('account/categories/show.html.twig', [
             'category' => $category,
         ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_categories_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Categories $category, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(CategoriesType::class, $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_public_categories_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('admin/categories/edit.html.twig', [
-            'category' => $category,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_categories_delete', methods: ['POST'])]
-    public function delete(Request $request, Categories $category, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($category);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_public_categories_index', [], Response::HTTP_SEE_OTHER);
     }
 }
